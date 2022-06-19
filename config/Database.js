57565d9e620fce7209f68b6ca82c2f7d39e796c4
septianppm <sag@ppm-manajemen.ac.id>
2022-06-19 23:13:20 +0700

    edit without port

--- config/Database.js
+++ config/Database.js
@@ -7,13 +7,13 @@ const {
   DB_PASSWORD,
   DB_USERNAME,
   DB_NAME,
-  DB_PORT,
+  //DB_PORT,
   DB_DIALECT
 } = process.env;
 
 const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
     host: DB_HOST,
-    port: DB_PORT,
+    //port: DB_PORT,
     dialect: DB_DIALECT
 });
  
